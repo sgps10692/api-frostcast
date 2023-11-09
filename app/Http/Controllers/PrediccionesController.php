1287d750d91c4668287a12b2edb4f4906637f6dc
Juan Pablo <jpsanabria37@soy.sena.edu.co>
2023-11-09 00:32:05 -0500

    cambios

--- app/Http/Controllers/PrediccionesController.php
+++ app/Http/Controllers/PrediccionesController.php
@@ -13,33 +13,39 @@ class PrediccionesController extends Controller
     public function getPrediccionesPorUbicacion($ubicacion_id){
 
         $ubicacion = Location::find($ubicacion_id);
-    
+
         if (!$ubicacion) {
             return response()->json(['error' => 'Ubicación no encontrada'], 404);
         }
     
-        // Obtener la predicción más reciente
-        $prediccion_reciente = $ubicacion->forecast_frosts()
-            ->latest('date')
-            ->first();
+        // Obtener los valores meteorológicos históricos para la ubicación
+        $historicalMeteorologicalValues = MeteorologicalValue::where('location_id', $ubicacion_id)
+            ->orderBy('date', 'asc')
+            ->with('parameter.variable')
+            ->get();
     
-        // Obtener los valores meteorológicos para la ubicación, incluyendo relaciones
-        $latestMeteorologicalValues = MeteorologicalValue::whereIn('id', function ($query) use ($ubicacion_id) {
-            $query->select(DB::raw('MAX(id)'))
-                ->from('meteorological_values')
-                ->where('location_id', $ubicacion_id)
-                ->groupBy('parameter_variable_id');
-        })
-        ->orderBy('date', 'desc') // Ordenar por fecha en orden descendente
-        ->with('parameter.variable')
-        ->get();
+        // Organizar los datos en un formato adecuado para el gráfico histórico
+        $chartData = [
+            'location' => $ubicacion,
+            'data' => [],
+        ];
     
+        foreach ($historicalMeteorologicalValues as $value) {
+            $variableName = $value->parameter->variable->name;
+            $parameterName = $value->parameter->name;
+            $date = $value->date;
+            $value = $value->value;
+    
+            // Agrupar los valores por variable y parámetro
+            $chartData['data'][$variableName][$parameterName][] = [
+                'date' => $date,
+                'value' => $value,
+            ];
+        }
     
         return response()->json([
             'success' => true,
-            'location' => $ubicacion,
-            'prediccion_reciente' =>  $prediccion_reciente,
-            'valores_meteorologicos' => $latestMeteorologicalValues
+            'chart_data' => $chartData,
         ]);
     }
     
